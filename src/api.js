5c3a91472d3cc272495de0dd9c40e9c97c81e7c9
Joylan <96838519+shanepai-14@users.noreply.github.com>
2024-04-30 04:43:12 +0800

    done

---
+++ src/api.js
@@ -0,0 +1,41 @@
+export const API_KEY =
+  'live_B7WKiVwpg0nCeeBlUPcCDGfP7ZE2paN1CCGtwQPnRxhhVEhajeJeAon7IZnKWn2z';
+export const BASE_URL = 'https://api.thecatapi.com/v1/';
+export const headers = new Headers({
+    "Content-Type": "application/json",
+    "x-api-key": API_KEY
+  });
+export function get_cats() {
+  return fetch(`${BASE_URL}breeds`, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response =>  {
+        if (!response.ok) {
+            throw new Error('Failed to fetch cats');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+      return {data};
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}images/search?breed_ids=${breedId}`, {
+      headers: headers
+    })
+      .then(response => 
+       { if (!response.ok) {
+            throw new Error('Failed to fetch cats');
+        }
+        return response.json();
+    }
+    )
+      .then(data => {
+          console.log(data);
+        return {data};
+      });
+  }
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import { get_cats,fetchCatByBreed } from './api';
+
+const loader = document.getElementById('loader');
+const error_display = document.getElementById('error');
+const cat_select = document.getElementById('cat_select');
+const cat_info = document.querySelector('.cat-info')
+
+cat_select.addEventListener('change', event => {
+    cat_info.innerHTML = '';
+    loader.classList.remove('hidden');
+        const breedId = event.target.value
+        fetchCatByBreed(breedId).then(({ data })=> {
+            console.log(data);
+          const description = data[0].breeds[0].description;
+          const name= data[0].breeds[0].name;
+          const url= data[0].url;
+
+          displayCatMarkup(url,description,name);
+          loader.classList.add('hidden');
+        }).catch(error => {
+            loader.classList.add('hidden');
+            error_display.classList.remove('hidden');
+        });
+});
+
+
+function displayCatMarkup(url,description,name){
+
+  const info = `<div 
+  class="max-w-lg mt-5  p-4 border border-gray-200 rounded shadow  md:p-6 ">
+  <div class="flex items-center w-full justify-center h-[400px] mb-4 bg-gray-300 rounded dark:bg-gray-400">
+   <img src="${url}" class="w-full h-full object-cover"> </img>
+  </div>
+  <div class="text-lg font-bold w-fit mb-4">${name}</div>
+  <div class="w-fit p-4 bg-gray-300   mb-2.5">
+   <p>${description}</p>
+  </div>
+
+</div>
+  `;
+ 
+  cat_info.insertAdjacentHTML('beforeend', info);
+    
+}
+
+
+function select_cats() {
+    return get_cats()
+    .then(({ data }) => {
+        const cats  = Object.values(data);
+         cats.map(cat => createSelectOptions(cat.name,cat.id))
+      });
+}
+function createSelectOptions(name,id){
+    console.log(name);
+    cat_select.insertAdjacentHTML('beforeend', `<option value="${id}">${name}</option>`);
+   
+}
+window.addEventListener('DOMContentLoaded', function () {
+    select_cats().catch(error => {
+        loader.classList.add('hidden');
+        error_display.classList.remove('hidden');
+    });;
+});
